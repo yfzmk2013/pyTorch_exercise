54db05f8b76003a05f6d6f69b47143a490d4f9e6
brenda1987 <yanhao_aiyj@163.com>
2018-04-30 17:34:50 +0800

    pytorch练习更新

--- basis/test_basis.py
+++ basis/test_basis.py
@@ -2,18 +2,30 @@
 # -*- coding: utf-8 -*-
 import torch
 
+import numpy as np
 def main():
-    print "we are in %s"%__name__
-    #y=torch.Tensor(4,2,3)
-    #print y
+    print "we are in %s" % __name__
+    # y=torch.Tensor(4,2,3)
+    # print y
     print torch.cuda.is_available()
-    x=torch.rand(4,3)
-    y=torch.rand(4,3)
-    print x,y
-    if(torch.cuda.is_available()):
+    x = torch.rand(4, 3)
+    y = torch.rand(4, 3)
+    print x, y
+    if (torch.cuda.is_available()):
         print "cuda is true!"
-        x=x.cuda()
-        y=y.cuda()
-    print(x+y)
+        x = x.cuda()
+        y = y.cuda()
+    print(x + y)
+    #x从gpu转cpu
+    x=x.cpu()
+    #tensor转numpy
+    z=x.numpy()
+    print z
+    print type(z)
+    #numpy转tensor
+    k = torch.from_numpy(z)
+    print type(k),k
+
+
 if __name__ == '__main__':
     main()
